dbbd21950c7999d6a9f4f9db9f7a36549e9b739e
fb0801 <fbhatti0801@gmail.com>
2024-03-01 18:07:18 +0000

    file encryption 2

--- goFileEncrption/main.go
+++ goFileEncrption/main.go
@@ -10,14 +10,17 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-func main(){
-	if len(os.Args) < 2{
+func main() {
+
+	// If not enough args, return help text
+	if len(os.Args) < 2 {
 		printHelp()
 		os.Exit(0)
 	}
+
 	function := os.Args[1]
 
-	switch function{
+	switch function {
 	case "help":
 		printHelp()
 	case "encrypt":
@@ -28,9 +31,10 @@ func main(){
 		fmt.Println("Run CryptoGo encrypt to encrypt a file, and CryptoGo decrypt to decrypt a file.")
 		os.Exit(1)
 	}
+
 }
 
-func printHelp(){
+func printHelp() {
 	fmt.Println("CryptoGo")
 	fmt.Println("Simple file encrypter for your day-to-day needs.")
 	fmt.Println("")
@@ -44,10 +48,10 @@ func printHelp(){
 	fmt.Println("\t decrypt\tTries to decrypt a file using a password")
 	fmt.Println("\t help\t\tDisplays help text")
 	fmt.Println("")
-
 }
 
 func encryptHandle() {
+
 	if len(os.Args) < 3 {
 		println("Missing the path to the file. For more information run CryptoGo help")
 		os.Exit(0)
@@ -64,9 +68,23 @@ func encryptHandle() {
 	fmt.Println("\nEncrypting...")
 	filecrypt.Encrypt(file, password)
 	fmt.Println("\nFile successfully protected")
+
 }
 
-func decryptHandle(){
+func getPassword() []byte {
+	fmt.Print("Enter password: ")
+	password, _ := terminal.ReadPassword(0)
+	fmt.Print("\nConfirm password: ")
+	password2, _ := terminal.ReadPassword(0)
+	if !validatePassword(password, password2) {
+		fmt.Print("\nPasswords do not match. Please try again.\n")
+		return getPassword()
+	}
+	return password
+}
+
+func decryptHandle() {
+
 	if len(os.Args) < 3 {
 		println("Missing the path to the file. For more information run CryptoGo help")
 		os.Exit(0)
@@ -87,19 +105,6 @@ func decryptHandle(){
 
 }
 
-func getPassword() []byte {
-	fmt.Print("Enter password: ")
-	password, _ := terminal.ReadPassword(0)
-	fmt.Print("\nConfirm password: ")
-	password2, _ := terminal.ReadPassword(0)
-	if !validatePassword(password, password2) {
-		fmt.Print("\nPasswords do not match. Please try again.\n")
-		return getPassword()
-	}
-	return password
-}
-
-
 func validatePassword(password1 []byte, password2 []byte) bool {
 	if !bytes.Equal(password1, password2) {
 		return false
@@ -108,7 +113,7 @@ func validatePassword(password1 []byte, password2 []byte) bool {
 	return true
 }
 
-func validateFile(){
+func validateFile(file string) bool {
 	if _, err := os.Stat(file); os.IsNotExist(err) {
 		return false
 	}
