ba3d9055fa4d04dc67efb2458a5df1233698c3c1
fb0801 <fbhatti0801@gmail.com>
2024-07-29 15:40:16 +0100

    API with DB 3

--- goFullstackApp/main.go
+++ goFullstackApp/main.go
@@ -1,14 +1,42 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
 
-import "fmt"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 type Todo struct {
-	ID int `json:"id"`
+	ID int `json:"_id" bson:"_id"`
 	Completed bool `json:"completed"`
 	Body string `json:"body"`
 }
 
+var collection *mongo.Collection
+
 func main(){
 	fmt.Println("hello world")
+
+	err:= godotenv.Load(".env")
+	if err != nil{
+		log.Fatal("Error loading .env file", err)
+	}
+
+	MONGODB_URI:= os.Getenv("MONGODB_URI")
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+	client,err := mongo.Connect(context.Background(), clientOptions)
+
+	if err != nil{
+		log.Fatal(err)
+	}
+	err = client.Ping(context.Background(), nil)
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB")
 }
