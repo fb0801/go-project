af012e5c5f93fc02aa1ff525f665ea2b653a9ca2
fb0801 <fbhatti0801@gmail.com>
2024-08-01 18:17:14 +0100

    consume API from react

--- goFullstackApp/client/src/App.tsx
+++ goFullstackApp/client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Container, Stack } from "@chakra-ui/react"
 import Navbar from "./components/Navbar"
 import TodoForm from "./components/TodoForm"
+import TodoList from "./components/TodoList"
 
 function App() {
  
--- goFullstackApp/client/src/main.tsx
+++ goFullstackApp/client/src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClient } from '@tanstack/react-query'
 
+
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
      <ChakraProvider>
